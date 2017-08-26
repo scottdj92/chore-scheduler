067bbc51a303fef9ae71a4797c7b91508abd306e
Scott Jones <scottdj92@gmail.com>
2017-08-26 16:18:25 -0400

    Implement basic calendar with momentjs

--- src/components/calendar/index.jsx
+++ src/components/calendar/index.jsx
@@ -1,11 +1,22 @@
 import React, { Component } from "react";
+import moment from "moment";
 
 import "./index.scss";
 
 export default class Calendar extends Component {
+  constructor() {
+    super();
+    this.state = {
+      currentTime: moment(),
+      selectedDay: ""
+    }
+  }
   render() {
+    console.log(this.state.currentTime);
     return(
-      <div>Calendar</div>
+      <section className='calendar'>
+        <h1 className='title is-2'>Calendar - {moment().format('ddd MMMM do YYYY')}</h1>
+      </section>
     );
   }
 }

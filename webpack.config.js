0d0d3f99f900a07b2caf4ff7af95dee61e0fa4fe
Scott Jones <Scott Jones>
2017-08-25 23:18:48 -0400

    Add fucking everything

---
+++ webpack.config.js
@@ -0,0 +1,66 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+const merge = require('webpack-merge');
+
+const PATHS = {
+  app: path.join(__dirname, 'src'),
+  dist: path.join(__dirname, 'dist'),
+  build: path.join(__dirname, 'build')
+};
+
+//import parts of webpack config
+const parts = require('./webpack.parts');
+
+const common = merge([
+  {
+    entry: {
+      app: PATHS.app
+    },
+    output: {
+      path: PATHS.dist,
+      filename: 'bundle.js'
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: 'index.html'
+      })
+    ]
+  }
+]);
+
+module.exports = function (env) {
+  if (env === 'prod') {
+    return merge([
+      common,
+      parts.clean(PATHS.build),
+      parts.loadJavaScript(PATHS.app),
+      parts.lintJavaScript({paths: PATHS.app}),
+      parts.extractCSS(),
+    ]);
+  }
+
+  return merge([
+    common,
+    {
+      plugins: [
+        new webpack.NamedModulesPlugin()
+      ]
+    },
+    parts.generateSourceMaps('eval-sourcemaps'),
+    // parts.compressImages(PATHS.app),
+    parts.devServer({
+      host: process.env.host,
+      port: process.env.port
+    }),
+    parts.loadJavaScript(PATHS.app),
+    parts.loadCSS(),
+    parts.lintJavaScript({
+      paths: PATHS.app,
+      options: {
+        emitWarning: true
+      }
+    }),
+    // parts.lintCSS(PATHS.app)
+  ]);
+}
---
+++ webpack.parts.js
@@ -0,0 +1,102 @@
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+exports.clean = function(path) {
+  return {
+    plugins: [
+      new CleanWebpackPlugin([path])
+    ]
+  }
+};
+
+exports.devServer = function(options) {
+  return {
+    devServer: {
+      historyApiFallback: true,
+      hot: true,
+      hotOnly: true,
+      stats: 'errors-only',
+      host: process.env.host,
+      port: process.env.port
+    },
+    plugins: [
+      new webpack.HotModuleReplacementPlugin()
+    ]
+  }
+};
+
+exports.lintJavaScript = function({ paths, options}) {
+  return {
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          enforce: 'pre',
+          loader: 'eslint-loader',
+          options: options
+        }
+      ]
+    }
+  }
+};
+
+exports.loadJavaScript = function(paths) {
+  return {
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          loader: 'babel-loader',
+          include: paths,
+          query: {
+            cacheDirectory: true,
+            presets: ['react', 'es2015']
+          }
+        }
+      ]
+    }
+  }
+};
+
+exports.loadCSS = function(paths) {
+  return {
+    devtool: 'source-map',
+    module: {
+      rules: [
+        {
+          test: /\.scss$/,
+          use: ['style-loader', 'css-loader', 'resolve-url-loader', 'sass-loader'],
+          include: paths
+        }
+      ]
+    }
+  }
+};
+
+exports.extractCSS = function(paths) {
+  return {
+    plugins: [
+      new ExtractTextPlugin({
+        filename: 'bundle.css'
+      })
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.scss$/,
+          use: ExtractTextPlugin.extract({
+            fallback: 'style-loader',
+            use: ['css-loader', 'resolve-url-loader', 'style-loader']
+          })
+        }
+      ]
+    }
+  }
+};
+
+exports.generateSourceMaps = function(type) {
+  return {
+    devtool: type
+  }
+};
